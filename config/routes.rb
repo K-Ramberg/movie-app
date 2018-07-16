367008aaec8ec51181ec66c348896dba653589ee
K-Ramberg <kramberg0350@gmail.com>
2018-07-15 20:24:51 -0400

    readme explains app break following devise goofup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'movies#index'
   resources :movies
   resources :users
--- db/migrate/20180715195931_add_api_id_to_movie.rb
+++
@@ -1,5 +0,0 @@
-class AddApiIdToMovie < ActiveRecord::Migration[5.2]
-  def change
-    add_column :movies, :api_id, :string
-  end
-end
